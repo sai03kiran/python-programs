7929d91f89c6248096d53922edb824547e22e11c
Sai Kiran <111441384+sai03kiran@users.noreply.github.com>
2022-09-05 13:29:25 +0530

    Delete simple interest calculator.py

--- simple interest calculator.py
+++
@@ -1,7 +0,0 @@
-def simple_interest(p,t,r):
-    print("The personal amount is:",p)
-    print("The time period is:",t)
-    print("The rate of interest is:",r)
-    a=(p*t*r)/100
-    print("The simple interest is:",a)
-simple_interest(8,6,8)
