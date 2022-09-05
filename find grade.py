f49211633b1fa47efc6fb54bf5ed17b5dfe4d0ee
Sai Kiran <111441384+sai03kiran@users.noreply.github.com>
2022-09-05 14:24:40 +0530

    Update find grade.py

--- find grade.py
+++ find grade.py
@@ -16,14 +16,8 @@ elif avg>=71 and avg<81:
 elif avg>=61 and avg<71:
     print("Your Grade is B2")
 elif avg>=51 and avg<61:
-    print("Your Grade is C1")
-elif avg>=41 and avg<51:
-    print("Your Grade is C2")
-elif avg>=33 and avg<41:
-    print("Your Grade is D")
-elif avg>=21 and avg<33:
-    print("Your Grade is E1")
-elif avg>=0 and avg<21:
-    print("Your Grade is E2")
+    print("Your Grade is C")
+elif avg>=0 and avg<51:
+    print("Your Grade is fail") 
 else:
     print("Invalid Input!")
