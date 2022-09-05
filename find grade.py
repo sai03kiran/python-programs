e98d069dd08dbb5d2694e05d61f442e77511342e
Sai Kiran <111441384+sai03kiran@users.noreply.github.com>
2022-09-05 14:07:39 +0530

    Add files via upload

---
+++ find grade.py
@@ -0,0 +1,28 @@
+print("Enter the marks of the student in 5 subjects:")
+a=int(input())
+b=int(input())
+c=int(input())
+d=int(input())
+e=int(input())
+tot=a+b+c+d+e
+avg=tot/5
+if avg>=91 and avg<=100:
+    print("Your Grade is A1")
+elif avg>=81 and avg<91:
+    print("Your Grade is A2")
+elif avg>=71 and avg<81:
+    print("Your Grade is B1")
+elif avg>=61 and avg<71:
+    print("Your Grade is B2")
+elif avg>=51 and avg<61:
+    print("Your Grade is C1")
+elif avg>=41 and avg<51:
+    print("Your Grade is C2")
+elif avg>=33 and avg<41:
+    print("Your Grade is D")
+elif avg>=21 and avg<33:
+    print("Your Grade is E1")
+elif avg>=0 and avg<21:
+    print("Your Grade is E2")
+else:
+    print("Invalid Input!")
