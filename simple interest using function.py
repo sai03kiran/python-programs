fb01426d8a12638f8d5a346869b2bb318007122b
Sai Kiran <111441384+sai03kiran@users.noreply.github.com>
2022-09-06 09:51:40 +0530

    Add files via upload

---
+++ simple interest using function.py
@@ -0,0 +1,7 @@
+def simple_interest(p,t,r):
+    print("enter the personal amount",p)
+    print("enter the time period",t)
+    print("enter the rate of interest",r)
+    a=(p*t*r)/100
+    print(a)
+simple_interest(8000,3,12)
