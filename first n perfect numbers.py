c5be458cf8c9ea3ad42119367a1558db9bd188d8
Sai Kiran <111441384+sai03kiran@users.noreply.github.com>
2022-09-07 13:27:06 +0530

    Delete first n perfect numbers.py

--- first n perfect numbers.py
+++
@@ -1,9 +0,0 @@
-a=int(input("Enter any number: "))
-print("The first",a,"perfect numbers are:")
-for Number in range(1,a- 1):
-    Sum = 0
-    for n in range(1, Number - 1):
-        if(Number % n == 0):
-            Sum = Sum + n
-    if(Sum == Number):
-        print("%d " %Number)
